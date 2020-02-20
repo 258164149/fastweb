d79e148ffd9f471d1add72283943073c99fe1ee7
xufs <258164149@qq.com>
2020-02-20 14:23:35 +0800

    Signed-off-by: xufs <258164149@qq.com>

--- base/demo/http/curl.php
+++ base/demo/http/curl.php
@@ -9,7 +9,7 @@ include_once "../../utils/HttpUtils.php";
 
 //"http://base.test.com/demo/http/get_img.php";
 $url = "https://mytest.xcx01.5067.org/get_img.php";
- HttpUtils::curl_upload($url,"E:/mywork/www/fastweb/base/files/ftp.txt");
+HttpUtils::curl_upload($url,"E:/mywork/www/fastweb/base/files/ftp.txt");
 //HttpUtils::curl_upload("http://base.test.com/demo/http/get_img.php","ftp.txt");
 
 
--- base/test.php
+++ base/test.php
@@ -1,5 +1,6 @@
 <?php
- 
+
+
 header("Content-Type: text/html;charset=utf-8");
 echo $str= '你好,这里是卖咖啡!';
 echo '<br />';
